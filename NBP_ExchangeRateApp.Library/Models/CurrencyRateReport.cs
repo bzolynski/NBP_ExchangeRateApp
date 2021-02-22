b4d5cfebc36008df81fc29e1e88ca17ba6c194fe
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2021-02-22 19:12:21 +0100

    Move models to separate files

--- NBP_ExchangeRateApp.Library/Models/CurrencyRateReport.cs
+++ NBP_ExchangeRateApp.Library/Models/CurrencyRateReport.cs
@@ -23,14 +23,6 @@ namespace NBP_ExchangeRateApp.Library.Models
         }
     }
 
-    public class Rate
-    {
-        public DateTime ReportDate { get; set; }
-        public string Currency { get; set; }
-        public string Code { get; set; }
-        public float Mid { get; set; }
-    }
-
     
 
 }
---
+++ NBP_ExchangeRateApp.Library/Models/Rate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NBP_ExchangeRateApp.Library.Models
+{
+    public class Rate
+    {
+        public DateTime ReportDate { get; set; }
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public float Mid { get; set; }
+    }
+
+    
+
+}
+
