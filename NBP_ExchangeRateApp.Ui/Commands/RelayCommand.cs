6f27032c0a3768cf989d67b60c972f479b4292df
Bartosz Żołyński <bartosz.zolynski@gmail.com>
2021-02-28 14:26:32 +0100

    Add RelayCommand

---
+++ NBP_ExchangeRateApp.Ui/Commands/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using NBP_ExchangeRateApp.Ui.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NBP_ExchangeRateApp.Ui.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
